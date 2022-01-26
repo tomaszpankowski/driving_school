41db3bc947c7305b3c0010914bd0f511a3bf16a3
tomaszpankowski <pankowski.email@gmail.com>
2022-01-26 08:21:01 +0100

    added backend

--- user.php
+++ user.php
@@ -1,3 +1,95 @@
+<?php
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();   
+}
+
+include_once "php/comm.php";
+include_once "php/db.php";
+include_once "php/t_message.php";
+include_once "php/t_user.php";
+
+//to remove after pub
+//include_once "php/support.php";
+//createAdminAccount("password","admin@mail.com","1234");
+
+if(isset($_POST["username"])
+&& isset($_POST["userpass"])){
+    DatabaseConnect();
+    $usr = new TUser($GLOBALS['connection']);   
+    $usr->getByName(htmlspecialchars($_POST["username"]));
+    if($usr->getData("username")===htmlspecialchars($_POST['username'])
+    && $usr->getData("password")===sha1(htmlspecialchars($_POST['userpass']))
+    ){
+        $_SESSION["UserLogged"] = $usr->getData("username");
+    }
+}
+
+if(isset($_SESSION["UserLogged"])){
+    //reading view config
+    if(isset($_POST["login"])){
+        $_SESSION["view"] = "dashboard";
+    }
+    if(isset($_POST["dashboard"])){
+        $_SESSION["view"] = "dashboard";
+    }
+    if(isset($_POST["messages"])){
+        $_SESSION["view"] = "messages";
+    }
+    if(isset($_POST["users"])){
+        $_SESSION["view"] = "users";
+    }
+    if(isset($_POST["edituser"])){
+        $_SESSION["view"] = "edituser";
+    }
+    if(isset($_POST["msginfo"])){
+        $_SESSION["view"] = "msginfo";
+    }
+    if(isset($_POST["msgsearch"])){
+        $_SESSION["view"] = "msgsearch";
+    }
+    if(isset($_POST["logout"])){
+        $_SESSION["view"] = "logout";
+    }
+    
+    //template selection and config
+    if(isset($_SESSION["view"])){
+        switch($_SESSION["view"]){
+            case "messages":
+                $_SESSION["viewTemplate"] = "templates/tmp_messages.php";
+                $_SESSION["CurrentPage"]=1;
+                break;
+            case "users":
+                $_SESSION["viewTemplate"] = "templates/tmp_users.php";
+                $_SESSION["CurrentPage"]=1;
+                break;
+            case "dashboard":
+                $_SESSION["viewTemplate"] = "templates/tmp_dashboard.php";
+                $_SESSION["CurrentPage"]=1;
+                break;
+            case "msginfo":
+                $_SESSION["viewTemplate"] = "templates/tmp_message_info.php";
+                $_SESSION["CurrentPage"]=1;
+                break;
+            case "msgsearch":
+                $_SESSION["viewTemplate"] = "templates/tmp_messages.php";
+                $_SESSION["CurrentPage"]=1;
+                break;
+            case "edituser":
+                $_SESSION["viewTemplate"] = "templates/tmp_edituser.php";
+                break;
+            default: 
+                $_SESSION["viewTemplate"] = "templates/tmp_login.php";     
+                $_SESSION = array();
+                session_destroy(); 
+        }
+    }
+}
+else{
+    $_SESSION["viewTemplate"] = "templates/tmp_login.php";
+}
+
+?>
 <!DOCTYPE html>
     <head>
         <meta charset="utf-8"/>
@@ -7,9 +99,9 @@
         <link rel="stylesheet" type="text/css" href="css/styles.css"/>
         <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css"/>
         <link rel="icon" href="img/favicon.png"/>
-        <title>Privacy policy</title>
+        <title> Driving school | User</title>
     </head>
-    <body class="minh-100vh bg-secondary">
+    <body class="minh-100vh">
         <header class="position-absolute w-100">
             <nav class="navbar navbar-dark navbar-expand-md bg-transparent">
                 <a href="index.html" class="navbar-brand ms-3">
@@ -45,53 +137,15 @@
             </nav>
         </header>
         <main>
-            <section class="privacy-s1 container-fluid d-flex minh-100vh align-items-center">
-                <div class="row mx-0 w-100 py-5 my-5">
-                    <div class="col-12 col-sm-9 col-md-7 col-lg-6 mx-auto">
-                        <div class="card text-center text-white bg-dark border-secondary shadow opacity-8">
-                            <div class="card-header">
-                                <h1 class="h4">
-                                    Privacy Policy
-                                </h1>
-                            </div>
-                            <div class="card-body px-4 text-start">
-                                <p class="mt-3">
-                                    This website is a demo version of real website, It doesn't collect and process, 
-                                    in long term meaning (longer than needed for website operation during visitor's 
-                                    presence), any user (visitor) data. All information collected during visitor's 
-                                    presence on this website is used only for technical purposes, required for correct 
-                                    operation of website or demonstration purposes related to technical mechanisms 
-                                    and presentation of its operation.
-                                </p>
-                                <p class="mt-3">
-                                    All data stored in cookies is directly related to saved visitor's settings 
-                                    for website's mechanisms, and can be managed by visitor with functionality 
-                                    provided by visitor's web browser - in order to get more information visitor 
-                                    should check web browser's manual.
-                                </p>
-                                <p class="mt-3">
-                                    However this website may contain attached third-party addons (like for 
-                                    example: scripts added by hosting services provider) which may collect 
-                                    visitors data for security and technical purposes. In order to know more 
-                                    about them, visitor should visit hosting provider's website and check 
-                                    privacy policy related to collected data.
-                                </p>
-                                <p class="mt-3">
-                                    All materials used for creation of this website were acquired from legal 
-                                    resources. Any convergences and similarities with any materials presented 
-                                    on other websites or resources are accidental.
-                                </p>
-                                <div class="card-footer border-top text-center">
-                                    <a href="index.html" class="btn btn-outline-light rounded-pill mt-3">
-                                        back to <span class="fw-bold">Home</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </main>
+            <?php
+            if(isset($_SESSION["viewTemplate"])){
+                include $_SESSION["viewTemplate"]; 
+            }
+            else{
+                include "templates/tmp_login.php";                            
+            }
+            ?>
+        </main>    
         <footer class="container-fluid d-flex text-dark align-items-center bg-dark text-white pt-3 opacity-9 border-top">
             <div class="row mx-0 w-100 small opacity-9">
                 <div class="col-12 col-md-5 text-center text-md-start">
@@ -141,6 +195,5 @@
         </footer>
         <script src="js/jquery-3.6.0.min.js"></script>
         <script src="js/bootstrap.bundle.min.js"></script>
-        <script src="js/main.js"></script>
     </body>
 </html>
